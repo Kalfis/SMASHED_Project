89fa5eb6d1afb8968451888bf19d6d86068309e8
William Chin <william-chin@outlook.com>
2015-11-06 09:20:33 -0500

    have users and sessions

--- app/controllers/smashes_controller.rb
+++ app/controllers/smashes_controller.rb
@@ -1,3 +1,33 @@
 class SmashesController < ApplicationController
+  def index
+    @smashes = Smash.all
+  end
 
+  def show
+    @smash = Smash.find(params[:id])
+  end
+
+  def
+    @smash = Smash.new
+  end
+
+  def create
+    @smash = Smash.new(smash_params)
+    @smash.user_id = current_user.sname
+    if @smash.save
+      redirect_to("/smashes")
+    else
+      render :new
+    end
+  end
+
+  private
+  def smash_params
+    params.require(:smash).permit(
+    :content
+    :image
+    :user
+    )
+  end
+  
 end
